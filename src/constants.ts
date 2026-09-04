e2563e5255dae6be8cf6bf179b72175f52f67f30
Laura 🐼 <64754203+lauvivans@users.noreply.github.com>
2026-09-04 00:25:59 -0300

    Fix(constants.ts): update gh profile username

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const GH_REPOS_URL = "https://api.github.com/users/lauravivan/repos";
-export const GH_REPO_URL = "https://api.github.com/repos/lauravivan/{repo-name}";
+export const GH_REPOS_URL = "https://api.github.com/users/lauvivans/repos";
+export const GH_REPO_URL = "https://api.github.com/repos/lauvivans/{repo-name}";
